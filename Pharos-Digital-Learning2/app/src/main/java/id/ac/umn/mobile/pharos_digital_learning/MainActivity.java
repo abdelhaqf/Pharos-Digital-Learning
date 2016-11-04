c2dc0375f7c30eace78582e00daef769f81cd4ab
Abdel Haq Firdausy <abdel_31354@yahoo.co.id>
2016-11-04 18:43:53 +0700

    test another activity

--- Pharos-Digital-Learning2/app/src/main/java/id/ac/umn/mobile/pharos_digital_learning/MainActivity.java
+++ Pharos-Digital-Learning2/app/src/main/java/id/ac/umn/mobile/pharos_digital_learning/MainActivity.java
@@ -1,7 +1,9 @@
 package id.ac.umn.mobile.pharos_digital_learning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void next(View v){
+        startActivity(new Intent(this,AnotherActivity.class));
     }
 }
